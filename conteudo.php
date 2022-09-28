25e4a928968400b7c0b1a3f050ca4f2591da65f3
Niayou <113477124+Niayou@users.noreply.github.com>
2022-09-28 14:50:46 -0300

    Add files via upload

---
+++ conteudo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conteudo</title>
+</head>
+<body>
+    <form action="resposta.php" method="POST">
+        Título:<input type="text" name="Título" placeholder = "Digite seu Email" ><br>
+        Conteúdo:<input type="text" name="Conteúdo" placeholder = "Digite sua Senha" ><br>
+        Autor:<input type="text" name="Autor" placeholder = "Digite sua Senha" ><br>
+        <input type="submit" name="Enviar"><br>
+    </form>
+</body>
+</html>
+
+    
